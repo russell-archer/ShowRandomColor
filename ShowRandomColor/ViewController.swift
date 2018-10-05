8d537b91073a5f60048bb6bccde77f0bda936ef3
russell-archer <russell.archer@mac.com>
2018-10-05 13:10:40 +0100

    Initial Commit

---
+++ ShowRandomColor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowRandomColor
+//
+//  Created by Russell Archer on 05/10/2018.
+//  Copyright © 2018 Russell Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
